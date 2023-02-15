c12ffb9bcb1f27276f0acd916c8c9d8b7c0066d9
Vlad Tsvigun <vl.tsv@outlook.com>
2023-02-15 22:30:42 +0200

    adding "Update" button WIP

--- lambda_function.py
+++ lambda_function.py
@@ -18,55 +18,90 @@ def lambda_handler(event, context):
         # Load the request body as a JSON object
         data = json.loads(event["body"])
 
-        # Get the text of the message and the chat ID
-        message = str(data["message"]["text"])
-        chat_id = data["message"]["chat"]["id"]
-        first_name = data["message"]["chat"]["first_name"]
+        if "callback_query" in data:
+            # Get the callback query data
+            callback_data = data["callback_query"]["data"]
+            chat_id = data["callback_query"]["message"]["chat"]["id"]
+            message_id = data["callback_query"]["message"]["message_id"]
 
-        # Check if the message starts with the word "ping"
-        if message.startswith("ping "):
-            # Split the message into words and get the second word (the IP address and port)
-            ip_port = message.split()[1]
+            response = f"Callback data received {callback_data=}"
+            data = {
+                "text": response.encode("utf8"),
+                "chat_id": chat_id,
+                "message_id": message_id,
+            }
 
-            # Try to validate the IP address
-            try:
-                ip = ip_port.split(":")[0]
-                ipaddress.ip_address(ip)
+            url = BASE_URL + "/editMessageText"
 
-                # Try to make a head request to the URL using the IP address and port
+            # Send the updated response back to the user
+            requests.post(url=url, data=data)
+
+        elif "message" in data:
+            # Get the text of the message and the chat ID
+            message = str(data["message"]["text"])
+            chat_id = data["message"]["chat"]["id"]
+            first_name = data["message"]["chat"]["first_name"]
+
+            # Check if the message starts with the word "ping"
+            if message.startswith("ping "):
+                # Split the message into words and get the second word (the IP address and port)
+                ip_port = message.split()[1]
+
+                # Try to validate the IP address
                 try:
-                    resp = requests.head(url=f"http://{ip_port}", timeout=5)
-                    response = f"IP address is reachable (:\n" \
-                               f"Status code: {resp.status_code}"
+                    ip = ip_port.split(":")[0]
+                    ipaddress.ip_address(ip)
 
-                except Exception:
-                    response = f"IP address is not reachable ):"
+                    keyboard = {
+                        "inline_keyboard": [
+                            [
+                                {
+                                    "text": "Update",
+                                    "callback_data": f"update {ip_port}"
+                                }
+                            ]
+                        ]
+                    }
 
-            except ValueError:
-                response = "Invalid <IP address>:<port>"
+                    # Try to make a head request to the URL using the IP address and port
+                    try:
+                        resp = requests.head(url=f"http://{ip_port}", timeout=5)
+                        response = f"IP address is reachable (:\n" \
+                                   f"Status code: {resp.status_code}"
 
-        # If the message starts with "/start"
-        elif message.startswith("/start"):
-            response = (
-                f"Hello, {first_name}!\n"
-                f"I am a simple bot that can ping an IP address.\n"
-                f"Please, send me a message in the following format:\n"
-                f"ping <IP address>:<port>"
-            )
+                    except Exception:
+                        response = f"IP address is not reachable ):"
+                        keyboard = None
 
-        # If the message doesn't match either "ping" or "start"
-        else:
-            response = "Send message in format 'ping <IP address>:<port>'"
+                except ValueError:
+                    response = "Invalid <IP address>:<port>"
+                    keyboard = None
 
-        # Prepare the data to send to the Telegram API
-        data = {
-            "text": response.encode("utf8"),
-            "chat_id": chat_id,
-        }
-        url = BASE_URL + "/sendMessage"
+            # If the message starts with "/start"
+            elif message.startswith("/start"):
+                response = (
+                    f"Hello, {first_name}!\n"
+                    f"I am a simple bot that can ping an IP address.\n"
+                    f"Please, send me a message in the following format:\n"
+                    f"ping <IP address>:<port>"
+                )
+                keyboard = None
 
-        # Send the response back to the user
-        requests.post(url=url, data=data)
+            # If the message doesn't match either "ping" or "start"
+            else:
+                response = "Send message in format 'ping <IP address>:<port>'"
+                keyboard = None
+
+            # Prepare the data to send to the Telegram API
+            data = {
+                "text": response.encode("utf8"),
+                "chat_id": chat_id,
+                "reply_markup": json.dumps(keyboard) if keyboard else None
+            }
+            url = BASE_URL + "/sendMessage"
+
+            # Send the response back to the user
+            requests.post(url=url, data=data)
 
     except Exception as e:
         print(e)
