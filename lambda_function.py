b0f1cc9aa0bed6a63a3d42f59faa1cc180ca08ce
Vlad Tsvigun <vl.tsv@outlook.com>
2023-02-18 11:07:53 +0200

    button now almost working

--- lambda_function.py
+++ lambda_function.py
@@ -4,7 +4,8 @@ import sys
 import ipaddress
 import requests
 
-# Get the path of the current file and append the vendored folder to the system path
+# Get the path of the current file and append
+# the vendored folder to the system path
 here = os.path.dirname(os.path.realpath(__file__))
 sys.path.append(os.path.join(here, "./vendored"))
 
@@ -24,7 +25,8 @@ def send_response(response, chat_id, keyboard=None, message_id=None):
     if keyboard is not None:
         data["reply_markup"] = json.dumps(keyboard)
 
-    # If a message ID is specified, use the editMessageText API method to update the message
+    # If a message ID is specified, use the editMessageText API
+    # method to update the message
     if message_id is not None:
         url = BASE_URL + "/editMessageText"
         data["message_id"] = message_id
@@ -36,77 +38,117 @@ def send_response(response, chat_id, keyboard=None, message_id=None):
     requests.post(url=url, data=data)
 
 
+def create_keyboard(ip_port, status):
+    return {
+        "inline_keyboard": [
+            [
+                {
+                    "text": "Update",
+                    "callback_data": f"{status} {ip_port}"
+                }
+            ]
+        ]
+    }
+
+
+def handle_callback_query(data):
+    # Get the callback query data
+    callback_data = data["callback_query"]["data"]
+    chat_id = data["callback_query"]["message"]["chat"]["id"]
+    message_id = data["callback_query"]["message"]["message_id"]
+
+    # if "up" in callback_data:
+    #     response = ""
+    # elif "down" in callback_data:
+    #     response = ""
+    # todo
+
+    response = f"Callback data received {callback_data=}"
+    send_response(response=response, chat_id=chat_id, message_id=message_id)
+
+
+def ping(ip_port):
+    # Try to make a head request to the URL using the IP address and port
+    try:
+        resp = requests.head(url=f"http://{ip_port}", timeout=5)
+        response = f"IP address is reachable (:\n" f"Status code: {resp.status_code}"
+        status = "up"
+
+    except Exception:
+        response = "IP address is not reachable ):"
+        status = "down"
+
+    return response, status
+
+
+def handle_ping_command(ip_port):
+    # Try to validate the IP address
+    try:
+        ip = ip_port.split(":")[0]
+        ipaddress.ip_address(ip)
+
+        response, status = ping(ip_port=ip_port)
+
+        keyboard = create_keyboard(ip_port=ip_port, status=status)
+
+    except ValueError:
+        response = "Invalid <IP address>:<port>"
+        keyboard = None
+
+    return response, keyboard
+
+
+def handle_start_command(first_name):
+    response = (
+        f"Hello, {first_name}!\n"
+        f"I am a simple bot that can ping an IP address.\n"
+        f"Please, send me a message in the following format:\n"
+        f"ping <IP address>:<port>"
+    )
+    keyboard = None
+    return response, keyboard
+
+
+def handle_default_message():
+    response = "Send message in format 'ping <IP address>:<port>'"
+    keyword = None
+    return response, keyword
+
+
+def handle_message(data):
+    # Get the text of the message and the chat ID
+    message = data["message"]["text"]
+    chat_id = data["message"]["chat"]["id"]
+    first_name = data["message"]["chat"]["first_name"]
+
+    # Check if the message starts with the word "ping"
+    if message.startswith("ping "):
+        # Split the message into words and get the second word
+        # (the IP address and port)
+        ip_port = message.split()[1]
+        response, keyboard = handle_ping_command(ip_port=ip_port)
+
+    # If the message starts with "/start"
+    elif message.startswith("/start"):
+        response, keyboard = handle_start_command(first_name)
+
+    # If the message doesn't match either "ping" or "start"
+    else:
+        response, keyboard = handle_default_message()
+
+    send_response(response=response, chat_id=chat_id, keyboard=keyboard)
+
+
 def lambda_handler(event, context):
     try:
         # Load the request body as a JSON object
         data = json.loads(event["body"])
 
-        if "callback_query" in data:
-            # Get the callback query data
-            callback_data = data["callback_query"]["data"]
-            chat_id = data["callback_query"]["message"]["chat"]["id"]
-            message_id = data["callback_query"]["message"]["message_id"]
+        if "message" in data:
+            handle_message(data)
 
-            response = f"Callback data received {callback_data=}"
-            send_response(response=response, chat_id=chat_id, message_id=message_id)
-
-        elif "message" in data:
-            # Get the text of the message and the chat ID
-            message = data["message"]["text"]
-            chat_id = data["message"]["chat"]["id"]
-            first_name = data["message"]["chat"]["first_name"]
-
-            keyboard = None
-
-            # Check if the message starts with the word "ping"
-            if message.startswith("ping "):
-                # Split the message into words and get the second word (the IP address and port)
-                ip_port = message.split()[1]
-
-                # Try to validate the IP address
-                try:
-                    ip = ip_port.split(":")[0]
-                    ipaddress.ip_address(ip)
-
-                    # Try to make a head request to the URL using the IP address and port
-                    try:
-                        resp = requests.head(url=f"http://{ip_port}", timeout=5)
-                        response = f"IP address is reachable (:\n" \
-                                   f"Status code: {resp.status_code}"
-                        status = "up"
-
-                    except Exception:
-                        response = f"IP address is not reachable ):"
-                        status = "down"
-
-                    keyboard = {
-                        "inline_keyboard": [
-                            [
-                                {
-                                    "text": "Update",
-                                    "callback_data": f"{ip_port} {status}"
-                                }
-                            ]
-                        ]
-                    }
-
-                except ValueError:
-                    response = "Invalid <IP address>:<port>"
-
-            # If the message starts with "/start"
-            elif message.startswith("/start"):
-                response = (
-                    f"Hello, {first_name}!\n"
-                    f"I am a simple bot that can ping an IP address.\n"
-                    f"Please, send me a message in the following format:\n"
-                    f"ping <IP address>:<port>"
-                )
-
-            # If the message doesn't match either "ping" or "start"
-            else:
-                response = "Send message in format 'ping <IP address>:<port>'"
-
-            send_response(response=response, chat_id=chat_id, keyboard=keyboard)
+        elif "callback_query" in data:
+            handle_callback_query(data)
 
     except Exception as e:
         print(e)
